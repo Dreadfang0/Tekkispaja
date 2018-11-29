8ef1a4199f7465727f1593956a18e00a676d5ead
Xenathyr <44671470+Xenathyr@users.noreply.github.com>
2018-11-29 16:37:40 +0200

    Teknologiapaja progress 29.11
    12:30 -> 16:40

---
+++ Virtual-avatar_project/Assets/Scripts/CClothesScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CClothesScript : MonoBehaviour {
+
+    public SpriteRenderer head;
+    public SpriteRenderer top;
+    public SpriteRenderer jacket;
+    public SpriteRenderer legs;
+
+    public int whatJacket = 1;
+    public int whatTop = 1;
+    public int whatLegs = 1;
+    public int whatHead = 1;
+    
+
+    void Update()
+    {
+        if(whatJacket == 1)
+        {
+            
+        }
+        {
+
+        }
+    }
+
+}
--- Virtual-avatar_project/Assets/Scripts/ChangingMenuScript.cs
+++ Virtual-avatar_project/Assets/Scripts/ChangingMenuScript.cs
@@ -6,20 +6,138 @@ using UnityEngine.UI;
 
 public class ChangingMenuScript : MonoBehaviour {
 
+    //Panel Objects
     public GameObject panel; //Lazy, this is the default (Hair panel)
     public GameObject beardpanel;
     public GameObject toppanel;
+    public GameObject jacketpanel;
     public GameObject legspanel;
     public GameObject glovespanel;
     public GameObject accpanel;
     public GameObject skincolorpanel;
     public GameObject haircolorpanel;
-    
+    // Panel Objects ABOVE
+
+    //Code for Hair buttons
+    public SpriteRenderer hair;
+    public Sprite[] hairs;
+    public int whathair = 1;
+    public void Changehair(int index)
+    {
+        whathair = index;
+    }
+    //Code for Beard buttons
+    public SpriteRenderer beard;
+    public Sprite[] beards;
+    public int whatbeard = 1;
+    public void Changebeard(int index)
+    {
+        whatbeard = index;
+    }
+    //Code for Shirt buttons
+    public SpriteRenderer shirt;
+    public Sprite[] shirts;
+    public int whatshirt = 1;
+    public void ChangeShirt(int index)
+    {
+        whatshirt = index;
+    }
+    //Code for Jacket buttons
+    public SpriteRenderer jacket;
+    public Sprite[] jackets;
+    public int whatJacket = 1;
+    public void ChangeJacket(int index)
+    {
+        whatJacket = index;
+    }
+    //Code for Legs buttons
+    public SpriteRenderer leg;
+    public Sprite[] legs;
+    public int whatleg = 1;
+    public void Changeleg(int index)
+    {
+        whatleg = index;
+    }
+    //Code for Gloves buttons
+    public SpriteRenderer glove;
+    public Sprite[] gloves;
+    public int whatglove = 1;
+    public void Changeglove(int index)
+    {
+        whatglove = index;
+    }
+    //Code for Accessories buttons
+    public SpriteRenderer acc;
+    public Sprite[] accs;
+    public int whatacc = 1;
+    public void Changeacc(int index)
+    {
+        whatacc = index;
+    }
+    //Code for SC/HC ??
 
     public void Update()
     {
-        
+        for (int i = 0; i < hairs.Length; i++) // HAIR CODE
+        {
+            if(i == whathair)
+            {
+                hair.sprite = hairs[i];
+            }
+
+        }
+        for (int i = 0; i < jackets.Length; i++) // JACKET CODE
+        {
+            if (i == whatJacket)
+            {
+                jacket.sprite = jackets[i];
+            }
+        }
+        for(int o = 0; o < beards.Length; o++) //BEARD CODE
+        {
+            if (o == whatbeard)
+            {
+                beard.sprite = beards[o];
+            }
+        }
+        for (int o = 0; o < shirts.Length; o++)
+        {
+            if (o == whatshirt)
+            {
+                shirt.sprite = shirts[o];
+            }
+        }
+        for (int o = 0; o < legs.Length; o++) //LEGS CODE
+        {
+            if (o == whatleg)
+            {
+                leg.sprite = legs[o];
+            }
+        }
+
+        for (int o = 0; o < gloves.Length; o++) //Gloves CODE
+        {
+            if (o == whatglove)
+            {
+                glove.sprite = gloves[o];
+            }
+        }
+
+        for (int o = 0; o < accs.Length; o++) //Accessories CODE
+        {
+            if (o == whatacc)
+            {
+                acc.sprite = accs[o];
+            }
+        }
+
+
+
+
     }
+   
+    
+    
     //Opening and closing the menu-sections, made without arrays = have fun, its actual DOGESHIT
     public void OpenPanel()
     {
@@ -31,6 +149,7 @@ public class ChangingMenuScript : MonoBehaviour {
         accpanel.SetActive(false);
         skincolorpanel.SetActive(false);
         haircolorpanel.SetActive(false);
+        jacketpanel.SetActive(false);
     }
     public void ClosePanels() // DEBUG ; Close all panels
     {
@@ -42,13 +161,13 @@ public class ChangingMenuScript : MonoBehaviour {
         accpanel.SetActive(false);
         skincolorpanel.SetActive(false);
         haircolorpanel.SetActive(false);
-    }
-    
-
+        jacketpanel.SetActive(false);
+    } 
     public void OpenPanelBeard()
     {
         panel.SetActive(false);
         toppanel.SetActive(false);
+        jacketpanel.SetActive(false);
         legspanel.SetActive(false);
         glovespanel.SetActive(false);
         accpanel.SetActive(false);
@@ -56,12 +175,11 @@ public class ChangingMenuScript : MonoBehaviour {
         haircolorpanel.SetActive(false);
         beardpanel.SetActive(true); //HERE!
     }
-    
-
     public void OpenPanelTop()
     {
         panel.SetActive(false);
         beardpanel.SetActive(false);
+        jacketpanel.SetActive(false);
         legspanel.SetActive(false);
         glovespanel.SetActive(false);
         accpanel.SetActive(false);
@@ -69,62 +187,72 @@ public class ChangingMenuScript : MonoBehaviour {
         haircolorpanel.SetActive(false);
         toppanel.SetActive(true); //HERE!
     }
-   
+    public void OpenPanelJacket()
+    {
+        panel.SetActive(false);
+        beardpanel.SetActive(false);
+        toppanel.SetActive(false);
+        jacketpanel.SetActive(true); //HERE
+        legspanel.SetActive(false);
+        glovespanel.SetActive(false);
+        accpanel.SetActive(false);
+        skincolorpanel.SetActive(false);
+        haircolorpanel.SetActive(false);
+    }
     public void OpenPanelLegs()
     {
         panel.SetActive(false);
         beardpanel.SetActive(false);
         toppanel.SetActive(false);
+        jacketpanel.SetActive(false);
         glovespanel.SetActive(false);
         accpanel.SetActive(false);
         skincolorpanel.SetActive(false);
         haircolorpanel.SetActive(false);
         legspanel.SetActive(true); //HERE!
     }
-    
-
     public void OpenPanelGloves()
     {
         panel.SetActive(false);
         beardpanel.SetActive(false);
         toppanel.SetActive(false);
+        jacketpanel.SetActive(false);
         legspanel.SetActive(false);
         accpanel.SetActive(false);
         skincolorpanel.SetActive(false);
         haircolorpanel.SetActive(false);
         glovespanel.SetActive(true); //HERE!
     }
-    
-
     public void OpenPanelAccesories()
     {
         panel.SetActive(false);
         beardpanel.SetActive(false);
         toppanel.SetActive(false);
+        jacketpanel.SetActive(false);
         legspanel.SetActive(false);
         glovespanel.SetActive(false);
         skincolorpanel.SetActive(false);
         haircolorpanel.SetActive(false);
         accpanel.SetActive(true); //HERE!
-    }
-    
+    } 
     public void OpenPanelSkincolor()
     {
         panel.SetActive(false);
         beardpanel.SetActive(false);
         toppanel.SetActive(false);
+        jacketpanel.SetActive(false);
         legspanel.SetActive(false);
         glovespanel.SetActive(false);
         accpanel.SetActive(false);
         skincolorpanel.SetActive(true); //HERE!
         haircolorpanel.SetActive(false);
     }
-   
     public void OpenPanelHaircolor()
     {
         panel.SetActive(false);
         beardpanel.SetActive(false);
         toppanel.SetActive(false);
+        jacketpanel.SetActive(false);
         legspanel.SetActive(false);
         glovespanel.SetActive(false);
         accpanel.SetActive(false);
