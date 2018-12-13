7afddb1485231da50259b9067f0f8f87477e3e76
Karhu Janne TTK18SP <janne-karhu@hotmail.com>
2018-12-13 15:22:59 +0200

    DBhandler script updates
    Should work with unity. Does not work with DB yet.

--- Virtual-avatar_project/Assets/Scripts/DBhandler.cs
+++ Virtual-avatar_project/Assets/Scripts/DBhandler.cs
@@ -3,51 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
-public class DBhandler : MonoBehaviour {
 
-    //Login form stuff
-    string hash = "InsertHashHere";
-    string formNick = "InsertNicknameHere";
-    string formPass = "InsertFormPasswordHerer";
-    WWWForm form;
-    private WWW www;
-    string formText;
-    string passWarning;
+/*
+ Original Creator and the Curser of this script: Janne Karhu TTK18S
+ All of this remains untested and unimplementd to this date so it might or might not work, but theoretically it should work.
+*/
+public class DBhandler : MonoBehaviour
+{
 
-    IEnumerator Login()
-    {
-        //Creates Form Connection
-        form = new WWWForm();
-        form.AddField("Myform_hash", hash); // Adds Hash to the field;
-        form.AddField("myformnick", formNick);
-        form.AddField("myFormPass", formPass);
-
-        //Calls server script
-        www = new WWW("insert address here/scripts/check_user.php", form);
-        //Waits for the form to check PHP file, so the game wont hang
-        yield return www;
-
-        if (www.error != null)
-        {
-            //error getting the result
-            Debug.Log("Error 404 user script not found");
-        }
-        else
-        {
-            formText = www.text.Trim();
-            Debug.Log("Result: " + formText);
-            // If credentials are correct we log in.
-            if (formText == "Correct")
-            {
-                // Insert login code and stat calls here
-
-            }
-            www.Dispose();
-        }
-        formPass = ""; // Clear pass if wrong
-        passWarning = "wrong username or password";
-    }
-    //Insert UI Functions here
+    //Fetch the data from SQL Database somehow    
+    //Also saving the values to SQL Database might be a good idea to be implemented
 
 }
 public class Character
@@ -62,7 +27,7 @@ public class Character
     private float Charisma;
 
     // List of IDs for avatar bodyparts: Hair/hat, Face, beard, coat, shirt, pants with shoes and accessories. 
-    // IMPORTANT --> USE ENUMS FOR GETTING SPRITE IDs <-- IMPORTANT
+    // IDs are used in Get*InsertBodypartHere* for fetching the sprites.. sprites need to be named with numbers eg. 1, 2, 3 etc.
     private int Body;
     private int Hair;
     private int Face;
@@ -72,7 +37,8 @@ public class Character
     private int Gloves;
     private int Pants;
     private int Accessories;
-    
+    // Call the Set*whatevers when Fetching the values form DB.
+    // The Gets are should be used in the editor or whenever you need to use these values.
     public int GetID()
     {
         return ID;
@@ -146,17 +112,40 @@ public class Character
         Charisma = NewCha;
     }
 
+    // ID getters and setters for bodyparts 
+    // Remember to rename sprites to numbers in folders
     public int GetBody()
     {
+        Sprite bodySprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Body Type/"+ Body + ".png");
         return Body;
     }
     public void SetBody(int NewBody)
     {
         Body = NewBody;
+        
     }
 
     public int GetHair()
     {
+        // Checks the bodytype and changes the sprite from the proper bodytype according to ID.
+        
+        while (Body == 1)
+        {
+            //This line should hopefully work but i am not entirely sure since i have not been able to test it
+            Sprite hairSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Childlike/Hair/" + Hair + ".png");
+        }
+        while (Body == 2)
+        {
+            Sprite hairSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Female/Hair/" + Hair + ".png");
+        }
+        while (Body == 3)
+        {
+            Sprite hairSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Slim/Hair/" + Hair + ".png");
+        }
+        while (Body == 4)
+        {
+            Sprite hairSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Muscular/Hair/" + Hair + ".png");
+        }
         return Hair;
     }
     public void SetHair(int NewHair)
@@ -166,6 +155,22 @@ public class Character
 
     public int GetFace()
     {
+        while (Body == 1)
+        {
+            Sprite FaceSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Faces/" + Face + ".png");
+        }
+        while (Body == 2)
+        {
+            Sprite FaceSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Faces/" + Face + ".png");
+        }
+        while (Body == 3)
+        {
+            Sprite FaceSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Faces/" + Face + ".png");
+        }
+        while (Body == 4)
+        {
+            Sprite FaceSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Faces/" + Face + ".png");
+        }
         return Face;
     }
     public void SetFace(int NewFace)
@@ -175,6 +180,22 @@ public class Character
 
     public int GetBeard()
     {
+        while (Body == 1)
+        {
+            Sprite BeardSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Childlike/Beard/" + Beard + ".png");
+        }
+        while (Body == 2)
+        {
+            Sprite BeardSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Female/Beard/" + Beard + ".png");
+        }
+        while (Body == 3)
+        {
+            Sprite BeardSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Slim/Beard/" + Beard + ".png");
+        }
+        while (Body == 4)
+        {
+            Sprite BeardSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Muscular/Beard/" + Beard + ".png");
+        }
         return Beard;
     }
     public void SetBeard(int NewBeard)
@@ -184,6 +205,22 @@ public class Character
 
     public int GetCoat()
     {
+        while (Body == 1)
+        {
+            Sprite CoatSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Childlike/Coat/" + Coat + ".png");
+        }
+        while (Body == 2)
+        {
+            Sprite CoatSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Female/Coat/" + Coat + ".png");
+        }
+        while (Body == 3)
+        {
+            Sprite CoatSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Slim/Coat/" + Coat + ".png");
+        }
+        while (Body == 4)
+        {
+            Sprite CoatSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Muscular/Coat/" + Coat + ".png");
+        }
         return Coat;
     }
     public void SetCoat(int NewCoat)
@@ -193,6 +230,22 @@ public class Character
 
     public int GetShirt()
     {
+        while (Body == 1)
+        {
+            Sprite ShirtSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Childlike/Shirt/" + Shirt + ".png");
+        }
+        while (Body == 2)
+        {
+            Sprite ShirtSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Female/Shirt/" + Shirt + ".png");
+        }
+        while (Body == 3)
+        {
+            Sprite ShirtSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Slim/Shirt/" + Shirt + ".png");
+        }
+        while (Body == 4)
+        {
+            Sprite ShirtSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Muscular/Shirt/" + Shirt + ".png");
+        }
         return Shirt;
     }
     public void SetShirt(int NewShirt)
@@ -202,6 +255,22 @@ public class Character
 
     public int GetGloves()
     {
+        while (Body == 1)
+        {
+            Sprite GlovesSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Childlike/Gloves/" + Gloves + ".png");
+        }
+        while (Body == 2)
+        {
+            Sprite GlovesSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Female/Gloves/" + Gloves + ".png");
+        }
+        while (Body == 3)
+        {
+            Sprite GlovesSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Slim/Gloves/" + Gloves + ".png");
+        }
+        while (Body == 4)
+        {
+            Sprite GlovesSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Muscular/Gloves/" + Gloves + ".png");
+        }
         return Gloves;
     }
     public void SetGloves(int NewGloves)
@@ -211,6 +280,22 @@ public class Character
 
     public int GetPants()
     {
+        while (Body == 1)
+        {
+            Sprite PantsSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Childlike/Legs/" + Pants + ".png");
+        }
+        while (Body == 2)
+        {
+            Sprite PantsSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Female/Legs/" + Pants + ".png");
+        }
+        while (Body == 3)
+        {
+            Sprite PantsSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Slim/Legs/" + Pants + ".png");
+        }
+        while (Body == 4)
+        {
+            Sprite PantsSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Muscular/Legs/" + Pants + ".png");
+        }
         return Pants;
     }
     public void SetPants(int NewPants)
@@ -220,43 +305,26 @@ public class Character
 
     public int GetAccessories()
     {
+        while (Body == 1)
+        {
+            Sprite AccSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Childlike/Acc/" + Accessories + ".png");
+        }
+        while (Body == 2)
+        {
+            Sprite AccSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Female/Acc/" + Accessories + ".png");
+        }
+        while (Body == 3)
+        {
+            Sprite AccSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Slim/Acc/" + Accessories + ".png");
+        }
+        while (Body == 4)
+        {
+            Sprite AccSprite = Resources.Load<Sprite>("Assets/Sprites/Artti/Male Muscular/Acc/" + Accessories + ".png");
+        }
         return Accessories;
     }
     public void SetAccessories(int NewAccessories)
     {
         Accessories = NewAccessories;
     }
-}
-// Figure this shit out. How it should be done? 1 enum or enum per body part?
-enum Hair
-{
-    
-}
-enum Face
-{
-
-}
-enum Beard
-{
-
-}
-enum Coat
-{
-
-}
-enum Shirt
-{
-
-}
-enum Gloves
-{
-
-}
-enum Pants
-{
-
-}
-enum Accessories
-{
-
 }
